2d8ea3b11c36fa8f2a1b13698b63cc03d93bcd6e
Sarang Dagdu <sarang.dagdu@outlook.com>
2021-08-09 11:04:18 +0530

    added play and pause buttons
    Created two buttons :
    1) Play Button -> To Play a copyright free mp3 file. (Also resumes playback from
                                 the point where music was paused)
    2) Pause Button -> To Pause the song.

--- app/src/main/java/com/example/android/testmediaplayer/MainActivity.java
+++ app/src/main/java/com/example/android/testmediaplayer/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.android.testmediaplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    MediaPlayer mediaPlayer;
+    Button playButton;
+    Button pauseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mediaPlayer = MediaPlayer.create(this, R.raw.dubstep);
+        playButton = findViewById(R.id.play_button);
+        pauseButton = findViewById(R.id.pause_button);
+        playButton.setOnClickListener(this);
+        pauseButton.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+        if (v.getId() == R.id.play_button) {
+            mediaPlayer.start();
+
+        } else if (v.getId() == R.id.pause_button) {
+            mediaPlayer.pause();
+        }
     }
 }
